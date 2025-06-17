4de5044ff9f99815f4ef6d9982f62166d49c3a42
jhowzluk <jhonatanluksik@gmail.com>
2025-06-16 21:27:18 -0300

    feat: adicionando OT5

---
+++ OT5/funcoes.js
@@ -0,0 +1,41 @@
+//Exercício 1
+let largura = parseInt(prompt("Digite a largura (m2): "));
+let altura = parseInt(prompt("Digite a altura (m2): "));
+
+function calculaArea(largura, altura) {
+    return largura * altura;
+}
+
+console.log("Área do retângulo: " + calculaArea(largura, altura) + "m2");
+
+// //Exercício 2
+let num = parseInt(prompt("Digite um número: "));
+
+function verificaPar(num) {
+    if(num % 2 == 0) {
+        return "Par";
+    } else {
+        return "Ímpar";
+    }
+}
+
+console.log("O número digitado é: " + verificaPar(num));
+
+//Exercício 3
+let polegadas = parseInt(prompt("Digite o valor em polegadas: "));
+
+function calculaCentimetros(polegadas) {
+    return polegadas * 2.54;
+}
+
+console.log("O valor em centímetros é: " + calculaCentimetros(polegadas) + "cm");
+
+//Exercício 4
+let peso = parseInt(prompt("Digite o peso (kg): "));
+let alturacm = parseInt(prompt("Digite a altura (cm): "));
+
+function calculaIMC(peso, altura) {
+    return peso / (altura / 100) ** 2;
+}
+
+console.log("Seu IMC é: " + calculaIMC(peso, alturacm));
