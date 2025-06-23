24ae8912fdfe312f6222361ab5dd2b6b0324d971
jhowzluk <jhonatanluksik@gmail.com>
2025-06-23 20:54:42 -0300

    feat: adicionando OT8

---
+++ OT8/desafio1.js
@@ -0,0 +1,25 @@
+let myvar;
+myvar = 10;
+
+let soma = 15 + 8;
+soma += 1;
+soma *= 3;
+
+let unisenai = true;
+
+let comida = ['arroz', 'feijão', 'ovo'];
+console.log(comida[1]);
+
+if(soma === myvar) {
+    console.log('É igual');
+}
+
+if(soma <= myvar) {
+    console.log('É menor ou igual');
+}
+
+function divisao(num1, num2) {
+    return num1 / num2;
+}
+
+console.log('Divisão de 10 por 2: ' + divisao(10, 2));
