933df0e6870b59f937f50b862aa9283f4e87f924
jhowzluk <jhonatanluksik@gmail.com>
2025-06-27 15:29:44 -0300

    feat: atualizando OT9

--- OT9/set.js
+++ OT9/set.js
@@ -6,3 +6,12 @@ frutas.has('Maçã');
 
 frutas.delete('Laranja');
 
+frutas.clear();
+
+let numeros = new Set(1, 2, 3);
+
+console.log(numeros.size());
+
+for(let valor of numeros) {
+    console.log(valor);
+}
