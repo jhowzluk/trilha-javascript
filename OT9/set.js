93acada417101bfe817870ea3c8223d086532147
jhowzluk <jhonatanluksik@gmail.com>
2025-06-30 18:52:28 -0300

    feat: finalizando OT9

--- OT9/set.js
+++ OT9/set.js
@@ -8,10 +8,13 @@ frutas.delete('Laranja');
 
 frutas.clear();
 
-let numeros = new Set(1, 2, 3);
+let numeros = new Set(1, 2, 3, 3, 1);
 
 console.log(numeros.size());
 
 for(let valor of numeros) {
     console.log(valor);
 }
+
+let numerosUnicos = new Set(numeros);
+console.log([...numerosUnicos]);
